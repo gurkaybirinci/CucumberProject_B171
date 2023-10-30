9eb8cb936257c4ed72690fe76d56266442ca7d11
Gürkay Birinci <117175953+gurkaybirinci@users.noreply.github.com>
2023-10-30 23:36:42 +0300

    v2

--- src/test/java/techproed/hooks/Hooks.java
+++ src/test/java/techproed/hooks/Hooks.java
@@ -1,4 +1,22 @@
 package techproed.hooks;
 
+
+import io.cucumber.java.Before;
+import techproed.utilities.Driver;
+
+import java.time.Duration;
+
+import static techproed.base_url.BaseUrl.setup;
+
 public class Hooks {
+    @Before("@API")
+    public void before_api(){
+        setup();
+    }
+
+    @Before("@UI")
+    public void before_ui(){
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        Driver.getDriver().manage().window().maximize();
+    }
 }
